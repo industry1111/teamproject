db68c73ab59435497ccaa342a584d212991377ad
cyh6253 <dbsghk6253@naver.com>
2021-05-31 10:25:09 +0900

    iiii

--- src/com/board/action/WrittenReviewsAction.java
+++ src/com/board/action/WrittenReviewsAction.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpSession;
 
 import action.Action;
 import action.ActionForward;
+import dto.reviewDTO;
 
 
 
@@ -19,8 +20,12 @@ public class WrittenReviewsAction implements Action{
 		request.setCharacterEncoding("utf-8");
 		
 		HttpSession session=request.getSession();
-		System.out.println(request.getParameter("rating1"));
+		int product_num = (Integer) session.getAttribute("product_num");
 		
+		reviewDTO rdto = new reviewDTO();
+		rdto.setRating1(Integer.parseInt(request.getParameter("Rating1")));
+		rdto.setRating2(Integer.parseInt(request.getParameter("Rating2")));
+		rdto.setRating3(Integer.parseInt(request.getParameter("Rating3")));
 		
 		
 		request.setAttribute("center", "writtenReviews.jsp");
