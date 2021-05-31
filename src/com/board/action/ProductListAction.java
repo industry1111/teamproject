1c076f9f992e75c54a16a96f221326e7027bc0c9
gudrb <gudrb2640@naver.com>
2021-05-31 20:32:30 +0900

    123

--- src/com/board/action/ProductListAction.java
+++ src/com/board/action/ProductListAction.java
@@ -21,35 +21,19 @@ public class ProductListAction implements Action {
 		HttpSession session = request.getSession();
 		int member_num = (Integer) session.getAttribute("member_num");
 		
-		pagingDTO param = new pagingDTO();
-		if(request.getParameter("pageNum")!=null) {
-			param.setPageNum(request.getParameter("pageNum"));
-		}else {
-			param.setPageNum("1");
-		}
-		param.setPageSize(12);
 		
-		//AdminGoodsDAO객체 생성 후 getGoodsList()생성
 		boardDAO bDao = new boardDAO();
 		List<productDTO> list = bDao.getProductList(member_num);
 		
-//		param.setCount(bDao.getProductCount());
-		param.setCurrentPage(Integer.parseInt(param.getPageNum()));
-		System.out.println("=>>>>>>>>>>>>>1111  여기 ");
-		param.setStartRow((param.getCurrentPage()-1 ) * param.getPageSize());
-//		System.out.println(param.getCount() + " count / " + param.getStartPage() + "startpage() / " + param.getCurrentPage() + " currentPage() ");
 
-		System.out.println("=>>>>>>>>>>>>>2222222222  여기 ");
 
 		//requset영역에 저장
 		request.setAttribute("list", list);
 		request.setAttribute("center", "ProductList.jsp");
-		request.setAttribute("param", param);
 		ActionForward forward = new ActionForward();
 		//페이지이동(뷰페이지로이동)
 		forward.setPath("mypage.jsp");
 		forward.setRedirect(false);	
-		System.out.println("=>>>>>>>>>>>>>333333333 여기 ");
 		return forward;
 	}
 }
