e5950cf9b2c2aa3fad82afe864303c5bb6027ff6
hyemin <bg565@naver.com>
2021-06-03 19:04:59 +0900

    order check8

--- src/com/order/action/OrderFrontController.java
+++ src/com/order/action/OrderFrontController.java
@@ -1,5 +1,30 @@
 package com.order.action;
 
-public class OrderFrontController {
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class OrderFrontController extends HttpServlet{
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doProcess(request, response);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doProcess(request, response);
+	}
+	
+	protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+	
+	
 
 }
