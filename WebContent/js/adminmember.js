2cb66185305bc02cefa821e467d5b62ae8c8a786
travel <gudrb2640@naver.com>
2021-06-12 11:45:04 +0900

    adminmember.js

--- WebContent/js/adminmember.js
+++ WebContent/js/adminmember.js
@@ -1,28 +1,27 @@
 $(function() {
+	
+	
 	$(".edit_btn").on("click",function(){
 		$(this).parent().siblings('.state').removeAttr("disabled");
 
 	});
 	$(".ok_btn").on("click",function(){
-
+		
 		var member_num = $(this).parent().parent().siblings('.member_num').val();
 		var member_code = $(this).parent().siblings('.state').children('option:selected').val();
 	
 		console.log("member_num : "+member_num);
 		console.log("member_code : "+member_code);
 		$(this).parent().siblings('.state').attr("disabled",true);
-
 		$.ajax({
 			
 			type : "post",
 			async : true,
-
 			url : contextPath + "/MemberStateUpdate.do",
 			data : {
 				
 				member_num : member_num,
 				member_code : member_code
-
 			},
 			dataType:"text",
 			success : function(){
