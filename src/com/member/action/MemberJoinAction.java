fafb572de97418bd69c3b9bd4c2ad53b6cbd5fdc
gudrb <gudrb2640@naver.com>
2021-06-14 10:53:13 +0900

    memberLogin

--- src/com/member/action/MemberJoinAction.java
+++ src/com/member/action/MemberJoinAction.java
@@ -64,7 +64,7 @@ public class MemberJoinAction implements Action{
 
 		forward.setRedirect(true);
 	
-		forward.setPath("./ReportLogin.st?);
+		forward.setPath("MemberLogin.me");
 
 		return forward;
 	}
