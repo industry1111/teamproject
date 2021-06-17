9a64f51a4dbc80c7715a5ef28427a95dedc62dc9
hyemin <bg565@naver.com>
2021-06-17 12:29:31 +0900

    productadd check1

--- WebContent/js/productadd.js
+++ WebContent/js/productadd.js
@@ -1,41 +1,11 @@
 $(function() {
-	
-	var numCheck = /[0-9]/;
-	var count_check = 0;
-	var count_price = 0;
-	
-	$("#product_count").blur(function() {
-		
-		var count = $(this).val();
-		
-		if($('#product_count').val() == '' || numCheck.test(count)){
-			count_check = 1;
-		}else{
-			alert("숫자만 작성해 주세요");
-		}
-		
-		
-	});
-	
-	$("#product_price").blur(function() {
-		
-		var price = $(this).val();
-		
-		if($('#product_price').val() == '' || numCheck.test(price)){
-			price_check = 1;
-		}else{
-			alert("숫자만 작성해 주세요");
-		}
-		
-		
-	});
-	
+
 
 	$("#add_btn").on("click",function(){
 		
 		if(!$('#category1 > option:selected').val() || !$('#category2 > option:selected').val() || !$('#category3 > option:selected').val() || 
 				!$('#brand_name > option:selected').val() || $('#product_name').val() == '' ||  $('#product_description').val() == '' 
-			|| $('#product_count').val() == '' ||  $('#product_price').val() == '' ) {
+			|| $('#product_count').val() == '' ||  $('#product_price').val() == '' || $("#product_img").val() == '' ) {
 		    
 			alert("옵션을 제대로 등록해주세요");
 	
